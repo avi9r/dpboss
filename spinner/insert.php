e6cac6a57f275653aae9146522ff68fdf339051f
avi9r <avinashranjan633@gmail.com>
2021-01-21 12:58:52 +0530

    dcd

--- spinner/insert.php
+++ spinner/insert.php
@@ -14,5 +14,5 @@ $check = "Select count(*) as num from spin_result where user_id ='$_SESSION[id]'
         $query = "INSERT INTO `spin_result`(`user_id`, `result`, `date`) VALUES('$author','$lottery', '$t')";
         $result = mysqli_query($db, $query) or die(mysqli_error($db));
     }elseif($data['num'] > 0){
-        echo "Try Again Tomorrow..!";
+        echo "Try Again Tomorrow..! One Spin Per Day..";
     }
